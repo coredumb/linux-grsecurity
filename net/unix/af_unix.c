9959b0e69e00fcd169d894149ed92304398969c0
Olivier Mauras <olivier@mauras.ch>
2015-03-03 11:45:37 +0100

    Apply af_unix-no_dead_poll.diff from minipli

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -2262,7 +2262,7 @@ static unsigned int unix_dgram_poll(struct file *file, struct socket *sock,
 	writable = unix_writable(sk);
 	other = unix_peer_get(sk);
 	if (other) {
-		if (unix_peer(other) != sk) {
+		if (!sock_flag(other, SOCK_DEAD) && unix_peer(other) != sk) {
 			sock_poll_wait(file, &unix_sk(other)->peer_wait, wait);
 			if (unix_recvq_full(other))
 				writable = 0;
